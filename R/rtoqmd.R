60ae029afbb3adbeb1a92ffafef57dfa71fe5588
Damien Dotta <damien.dotta@live.fr>
2025-11-26 14:59:25 +0100

    Init

---
+++ R/rtoqmd.R
@@ -0,0 +1,16 @@
+#' Convert R Script to Quarto Markdown
+#'
+#' This function converts an R script to Quarto markdown format.
+#'
+#' @param input_file Path to the input R script file
+#' @param output_file Path to the output Quarto markdown file (optional)
+#' @return NULL
+#' @export
+#' @examples
+#' \dontrun{
+#' rtoqmd("script.R", "output.qmd")
+#' }
+rtoqmd <- function(input_file, output_file = NULL) {
+  # TODO: Implement conversion logic
+  message("Converting R script to Quarto markdown...")
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# quartoify
+
+## Description
+
+[Add a description of your package here]
+
+## Installation
+
+You can install the development version of quartoify from GitHub:
+
+```r
+# install.packages("devtools")
+devtools::install_github("ddotta/quartoify")
+```
+
+## Usage
+
+```r
+library(quartoify)
+
+# Convert an R script to Quarto markdown
+rtoqmd("script.R", "output.qmd")
+```
+
+## License
+
+MIT
